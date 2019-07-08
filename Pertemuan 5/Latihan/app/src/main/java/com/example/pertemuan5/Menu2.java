302b403417fd09bead3d0f4d3b8543c212e8734f
rzcyber <rzcyber@gmail.com>
2019-07-08 17:04:54 +0800

    Pertemuan 5

---
+++ Pertemuan 5/Latihan/app/src/main/java/com/example/pertemuan5/Menu2.java
@@ -0,0 +1,13 @@
+package com.example.pertemuan5;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Menu2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu2);
+    }
+}
