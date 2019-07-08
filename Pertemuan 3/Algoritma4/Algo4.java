b863252891a1d6922901d4e96209314ae7ed4749
rzcyber <rzcyber@gmail.com>
2019-07-08 12:26:26 +0800

    Pertemuan 3

--- Pertemuan 3/Algoritma4/Algo4.java
+++ Pertemuan 3/Algoritma4/Algo4.java
@@ -1,11 +1,10 @@
+// algo4
 import java.util.*;
 
 public class Algo4{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-
 		int x, y;
-
 		System.out.print("Masukkan nilai x = ");
 		x = sc.nextInt();
 		System.out.print("Masukkan nilai y = ");
