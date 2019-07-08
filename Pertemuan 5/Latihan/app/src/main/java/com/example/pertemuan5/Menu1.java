b9b941c8e88c397191df240b750043eb3bd5f639
rzcyber <rzcyber@gmail.com>
2019-07-08 18:55:25 +0800

    Pertemuan 5

--- Pertemuan 5/Latihan/app/src/main/java/com/example/pertemuan5/Menu1.java
+++ Pertemuan 5/Latihan/app/src/main/java/com/example/pertemuan5/Menu1.java
@@ -2,13 +2,30 @@ package com.example.pertemuan5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu1 extends AppCompatActivity {
 
+    //deklarasi object java
+    Button BtnOK;
+    Button BtnOK2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu1);
+
+        //hubungkan object java dengan ID XML
+        BtnOK = findViewById(R.id.Btn1);
+        BtnOK2 = findViewById(R.id.Btn2);
+    }
+    //code merubah warna
+    public void Rubah_Warna(View v){
+        BtnOK.setBackgroundColor(Color.RED);
+    }
+    public void Rubah_Warna1(View v){
+        BtnOK2.setBackgroundColor(Color.GREEN);
     }
 }
--- Pertemuan 5/Latihan/app/src/main/java/com/example/pertemuan5/Menu2.java
+++ Pertemuan 5/Latihan/app/src/main/java/com/example/pertemuan5/Menu2.java
@@ -1,6 +1,7 @@
 package com.example.pertemuan5;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class Menu2 extends AppCompatActivity {
