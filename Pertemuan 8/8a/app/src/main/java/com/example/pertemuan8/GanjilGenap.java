0463daee05485b514c77c6382056b6d5bdda1f6d
rzcyber <rzcyber@gmail.com>
2019-07-11 16:33:44 +0800

    Pertemuan 8

---
+++ Pertemuan 8/8a/app/src/main/java/com/example/pertemuan8/GanjilGenap.java
@@ -0,0 +1,14 @@
+package com.example.pertemuan8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GanjilGenap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ganjil_genap);
+    }
+}
